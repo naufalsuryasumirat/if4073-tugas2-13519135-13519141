781569eeba2d90f5a57e19673a9f2e4109a46634
ayahyaaa <k.naufalyahya@gmail.com>
2022-10-04 06:35:37 +0700

    feat: add periodic noise reduction 2nd photo

--- src/n06_noise_reduction.m
+++ src/n06_noise_reduction.m
@@ -1,3 +1,7 @@
 img = imread('img/lenanoise.png');
 newimg = noise_reduction(img, 'lena1');
 figure, imshowpair(img, newimg, 'montage');
+
+img = imread('img/scientificnoise.png');
+newimg = noise_reduction(img, 'science');
+figure, imshowpair(img, newimg, 'montage');
--- src/utils/noise_reduction.m
+++ src/utils/noise_reduction.m
@@ -16,7 +16,7 @@ function newimg = noise_reduction(img,type)
     imgf = fftshift(fftn(im2double(img)));
     figure; imagesc(log1p(abs(imgf))), colormap gray;
 
-    if type == 'lena1'
+    if strcmp(type,'lena1')
         indexes = [
             22 123 26 127;
             209 103 212 107;
@@ -36,7 +36,30 @@ function newimg = noise_reduction(img,type)
             range = indexes(i,:);
             imgf(range(1):range(3),range(2):range(4)) = 0;
         end
+    elseif strcmp(type,'science')
+        indexes = [
+            1 320 345 324;
+            1 440 345 460;
+            540 320 881 324;
+            540 440 881 460;
+            441 1 441 320;
+            441 579 441 881;
+            1 577 345 579;
+            540 577 881 579;
+            335 126 350 126;
+            335 191 350 192;
+            335 256 350 258;
+            335 780 350 780;
+            335 697 350 697;
+            335 713 350 715;
+            335 843 350 846;
+            335 649 350 650;
+        ];
 
+        for i=1:size(indexes,1)
+            range = indexes(i,:);
+            imgf(range(1):range(3),range(2):range(4)) = 0;
+        end
     end
 
     figure; imagesc(log1p(abs(imgf))), colormap gray;
